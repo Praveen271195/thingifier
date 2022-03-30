0642b731ecd2c7773a6a3d5a27358e4544c34590
Praveen Chandrasekaran <pc2846@rit.edu>
2022-03-29 20:48:53 -0400

    Using equalIgnoreCase() is cleaner than using toUpperCase/toLowerCase()

--- ercoremodel/src/main/java/uk/co/compendiumdev/thingifier/core/domain/definitions/relationship/RelationshipDefinition.java
+++ ercoremodel/src/main/java/uk/co/compendiumdev/thingifier/core/domain/definitions/relationship/RelationshipDefinition.java
@@ -68,7 +68,7 @@ public class RelationshipDefinition {
     /*
         A Relationship is known as any of its vector names
      */
-    //TODO: Using equalIgnoreCase() is cleaner than using toUpperCase/toLowerCase().equals()
+    //TODO: Using equalIgnoreCase() is cleaner than using toUpperCase/toLowerCase()
     public boolean isKnownAs(String relationshipName) {
 
         if (fromTo.getName().toLowerCase().equalsIgnoreCase(relationshipName)) {
